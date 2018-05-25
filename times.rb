33dc32e9d9e46fac836fa49d99e58719b0b42fb9
Joana Palero <joana.palero24@gmail.com>
2018-05-25 19:22:02 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -1,5 +1,9 @@
 def using_times
-	#your code here
+	7.times do 
+  puts "Hiding 10 stolen jewels."
+  jewels_in_bag = jewels_in_bag - 10
+  puts "Now there are only #{jewels_in_bag} jewels left to hide!"
+end
 end
 
 
