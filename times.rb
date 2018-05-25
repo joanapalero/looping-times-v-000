0338236a994f405870c0a00349ca8bc15f55c2e4
Joana Palero <joana.palero24@gmail.com>
2018-05-25 19:22:14 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -1,6 +1,6 @@
 def using_times
 	7.times do 
-  puts "Hiding 10 stolen jewels."
+  puts "Wingardium Leviosa"
   jewels_in_bag = jewels_in_bag - 10
   puts "Now there are only #{jewels_in_bag} jewels left to hide!"
 end
